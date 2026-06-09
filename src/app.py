12c5055fcd019fbe790dd528361e67c5e8a4732b
ayoowo99-spec <ayoowo99@gmail.com>
2026-06-09 23:36:52 +0000

    Fix duplicate registration bug

--- src/app.py
+++ src/app.py
@@ -37,8 +37,14 @@ activities = {
         "description": "Physical education and sports activities",
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
-        "participants": ["john@mergington.edu", "olivia@mergington.edu"]
-    }
+        "participants": ["john@mergington.edu", "olivia@mergington.edu"],
+    },
+    "Drama Club": {
+            "description": "Perform in school plays and musicals",
+            "schedule": "Thursdays, 4:00 PM - 6:00 PM",
+            "max_participants": 20,
+            "participants": []
+        }
 }
 
 
